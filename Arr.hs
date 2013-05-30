cf56d94216f6250af3ae6494bd54fd873a86e94c
Bryan O'Sullivan <bos@serpentine.com>
2013-05-29 22:20:33 -0700

    Move Arr code into its own module

--- Arr.hs
+++ Arr.hs
@@ -3,10 +3,10 @@
 module Arr
     (
       Arr(..)
-    , newArr
-    , sizeArr
-    , readArr
-    , writeArr
+    , new
+    , size
+    , read
+    , write
     ) where
 
 import GHC.Base (($))
@@ -15,17 +15,17 @@ import GHC.Types (IO(..), Int(..))
 
 data Arr a = Arr (MutableArray# RealWorld a)
 
-newArr :: a -> Int -> IO (Arr a)
-newArr defval (I# n#) = IO $ \s0# ->
+new :: a -> Int -> IO (Arr a)
+new defval (I# n#) = IO $ \s0# ->
   case newArray# n# defval s0# of (# s1#, marr# #) -> (# s1#, Arr marr# #)
 
-sizeArr :: Arr a -> Int
-sizeArr (Arr a) = I# (sizeofMutableArray# a)
+size :: Arr a -> Int
+size (Arr a) = I# (sizeofMutableArray# a)
 
-readArr :: Arr a -> Int -> IO a
-readArr (Arr a) (I# n#) = IO $ \s0# ->
+read :: Arr a -> Int -> IO a
+read (Arr a) (I# n#) = IO $ \s0# ->
   case readArray# a n# s0# of (# s1#, val #) -> (# s1#, val #)
 
-writeArr :: Arr a -> Int -> a -> IO ()
-writeArr (Arr a) (I# n#) val = IO $ \s0# ->
+write :: Arr a -> Int -> a -> IO ()
+write (Arr a) (I# n#) val = IO $ \s0# ->
   case writeArray# a n# val s0# of s1# -> (# s1#, () #)
--- IntTable.hs
+++ IntTable.hs
@@ -22,18 +22,19 @@ module IntTable
     , updateWith
     ) where
 
-import Arr
+import Arr (Arr)
+import Control.Monad ((=<<), liftM, forM_)
+import Data.Bits
+import Data.IORef
 import Data.Maybe (Maybe(..))
+import Foreign.ForeignPtr
+import Foreign.Storable
 import GHC.Base (Monad(..), ($), const, otherwise)
 import GHC.Classes (Eq(..), Ord(..))
 import GHC.Num (Num(..))
 import GHC.Prim (seq)
 import GHC.Types (Bool(..), IO(..), Int(..))
-import Data.IORef
-import Data.Bits
-import Foreign.ForeignPtr
-import Foreign.Storable
-import Control.Monad ((=<<), liftM, forM_)
+import qualified Arr
 
 newtype IntTable a = IntTable (IORef (IT a))
 
@@ -58,14 +59,14 @@ lookup k (IntTable ref) = do
         | otherwise      = go bucketNext
       go _ = return Nothing
   it@IT{..} <- readIORef ref
-  go =<< readArr tabArr (indexOf k it)
+  go =<< Arr.read tabArr (indexOf k it)
 
 new :: Int -> IO (IntTable a)
 new capacity = IntTable `liftM` (newIORef =<< new_ capacity)
 
 new_ :: Int -> IO (IT a)
 new_ capacity = do
-  arr <- newArr Empty capacity
+  arr <- Arr.new Empty capacity
   size <- mallocForeignPtr
   withForeignPtr size $ \ptr -> poke ptr 0
   return IT { tabArr = arr
@@ -79,17 +80,17 @@ infixr 5 <>
 
 grow :: IT a -> IORef (IT a) -> Int -> IO ()
 grow oldit ref size = do
-  newit <- new_ (sizeArr (tabArr oldit) `shiftL` 1)
+  newit <- new_ (Arr.size (tabArr oldit) `shiftL` 1)
   let copyOuter n !i
         | n == size = return ()
         | otherwise = do
           let inner !m Empty         = copyOuter m (i+1)
               inner m bkt@Bucket{..} = do
                 let idx = indexOf bucketKey newit
-                next <- readArr (tabArr newit) idx
-                writeArr (tabArr newit) idx bkt { bucketNext = next }
+                next <- Arr.read (tabArr newit) idx
+                Arr.write (tabArr newit) idx bkt { bucketNext = next }
                 inner (m+1) bucketNext
-          inner n =<< readArr (tabArr oldit) i
+          inner n =<< Arr.read (tabArr oldit) i
   copyOuter 0 0
   withForeignPtr (tabSize newit) $ \ptr -> poke ptr size
   writeIORef ref newit
@@ -101,30 +102,30 @@ insertWith f k v inttable@(IntTable ref) = do
   let go seen bkt@Bucket{..}
         | bucketKey == k = do
           let !v' = f bucketValue v
-          writeArr tabArr idx (Bucket k v' (seen <> bucketNext))
+          Arr.write tabArr idx (Bucket k v' (seen <> bucketNext))
           return (Just bucketValue)
         | otherwise = go bkt { bucketNext = seen } bucketNext
       go seen _ = withForeignPtr tabSize $ \ptr -> do
         size <- peek ptr
-        if size + 1 >= sizeArr tabArr - (sizeArr tabArr `shiftR` 2)
+        if size + 1 >= Arr.size tabArr - (Arr.size tabArr `shiftR` 2)
           then grow it ref size >> insertWith f k v inttable
           else do
-            v `seq` writeArr tabArr idx (Bucket k v seen)
+            v `seq` Arr.write tabArr idx (Bucket k v seen)
             poke ptr (size + 1)
             return Nothing
-  go Empty =<< readArr tabArr idx
+  go Empty =<< Arr.read tabArr idx
 {-# INLINABLE insertWith #-}
 
 toList :: IntTable a -> IO [(Key, a)]
 toList (IntTable ref) = do
   IT{..} <- readIORef ref
   let outer acc i
-        | i >= sizeArr tabArr = return acc
+        | i >= Arr.size tabArr = return acc
         | otherwise = do
           let inner iacc Empty = outer iacc (i+1)
               inner iacc Bucket{..} =
                 inner ((bucketKey,bucketValue):iacc) bucketNext
-          inner acc =<< readArr tabArr i
+          inner acc =<< Arr.read tabArr i
   outer [] 0
 
 fromList :: [(Key, a)] -> IO (IntTable a)
@@ -134,7 +135,7 @@ fromList kvs = do
   return t
 
 indexOf :: Key -> IT a -> Int
-indexOf k IT{..} = k .&. (sizeArr tabArr - 1)
+indexOf k IT{..} = k .&. (Arr.size tabArr - 1)
 
 delete :: Key -> IntTable a -> IO (Maybe a)
 delete k t = updateWith (const Nothing) k t
@@ -151,7 +152,7 @@ updateWith f k (IntTable ref) = do
         | otherwise = case go changed bucketNext of
                         (ov, nb) -> (ov, bkt { bucketNext = nb })
       go _ e = (Nothing, e)
-  (oldVal, newBucket) <- go False `liftM` readArr tabArr idx
+  (oldVal, newBucket) <- go False `liftM` Arr.read tabArr idx
   case oldVal of
-    Just _ -> writeArr tabArr idx newBucket >> return oldVal
+    Just _ -> Arr.write tabArr idx newBucket >> return oldVal
     _      -> return oldVal
